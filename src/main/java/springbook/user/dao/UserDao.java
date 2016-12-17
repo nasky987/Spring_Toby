66fe582df884b49fdfe54ee6de2ea3010f33ac0a
JOHN LEE <nasky987@gmail.com>
2016-12-17 17:28:09 +0900

    #토비 스프링 따라치기
    - 58장 까지 기본 코드 작성

---
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -0,0 +1,48 @@
+package springbook.user.dao;
+
+import springbook.user.domain.User;
+
+import java.sql.*;
+
+/**
+ * Created by hreeman on 12/17/16.
+ */
+public class UserDao {
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        Class.forName("org.mariadb.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mariadb://mariadb.my.to:3306/toby_spring", "toby", "toby");
+        PreparedStatement preparedStatement = connection.prepareStatement("insert into users(id, name, password) values(?, ?, ?)");
+
+        preparedStatement.setString(1, user.getId());
+        preparedStatement.setString(2, user.getName());
+        preparedStatement.setString(3, user.getPassword());
+
+        preparedStatement.execute();
+
+        preparedStatement.close();
+        connection.close();
+    }
+
+    public User get(String id) throws ClassNotFoundException, SQLException {
+        Class.forName("org.mariadb.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mariadb://mariadb.my.to:3306/toby_spring", "toby", "toby");
+        PreparedStatement preparedStatement = connection.prepareStatement("select * from users where id = ?");
+
+        preparedStatement.setString(1, id);
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+
+        User user = new User();
+        user.setId(resultSet.getString("id"));
+        user.setName(resultSet.getString("name"));
+        user.setPassword(resultSet.getString("password"));
+
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+
+        return user;
+    }
+
+}
