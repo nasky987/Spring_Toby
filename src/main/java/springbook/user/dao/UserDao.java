4bd5102d7c29c01e92f546afae1dd10546e3e419
JOHN LEE <nasky987@gmail.com>
2016-12-25 22:46:35 +0900

    #토비 스프링 따라치기 - 스프링 싱글톤
    - UserDao의 일반적인 싱글톤 패턴을 사용할 때의 문제점을 수정한 코드
    - 인스턴스 변수를 이용하여, 다수의 스레드에서 요청이 들어와도 매번 새로운 정보를 담는 인스턴스 변수를 이용하여 해결
    - 스프링 빈의 기본 스코프는 싱글톤을 사용한다.
    - 싱글톤 스코프는 컨테이너 내의 한개의 오브젝트만 만들어져서 강제로 제거하지 않는 한, 스프링 컨테이너에 유지된다.
    - 경우에 따라 싱글톤 외에 프로토타입(prototype), 요청(request), 세션(session)스코프등을 사용할 수 있다.
    - 프로토타입 스코프는 요청시 매번 새로운 오브젝트를 생성 해주며
    - 요청 스코프는 웹을 통해 새로운 HTTP요청이 생길때마다 새로운 오브젝트를 생성해주며
    - 세션 스코프는 웹의 세션과 유사하다.

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -8,19 +8,12 @@ import java.sql.*;
  * Created by hreeman on 12/17/16.
  */
 public class UserDao {
-    private static UserDao INSTANCE;
     private ConnectionMaker connectionMaker;
+    private Connection connection;
+    private User user;
 
-    private UserDao(ConnectionMaker connectionMaker) {
+    public UserDao(ConnectionMaker connectionMaker) {
         this.connectionMaker = connectionMaker;
-    }
-
-    public static synchronized UserDao getInstance() {
-        if (INSTANCE == null) {
-            INSTANCE = new UserDao(???);
-
-            return INSTANCE;
-        }
     }
 
     public void add(User user) throws ClassNotFoundException, SQLException {
@@ -38,7 +31,7 @@ public class UserDao {
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Connection connection = connectionMaker.makeConnection();
+        this.connection = connectionMaker.makeConnection();
         PreparedStatement preparedStatement = connection.prepareStatement("select * from users where id = ?");
 
         preparedStatement.setString(1, id);
@@ -46,15 +39,15 @@ public class UserDao {
         ResultSet resultSet = preparedStatement.executeQuery();
         resultSet.next();
 
-        User user = new User();
-        user.setId(resultSet.getString("id"));
-        user.setName(resultSet.getString("name"));
-        user.setPassword(resultSet.getString("password"));
+        this.user = new User();
+        this.user.setId(resultSet.getString("id"));
+        this.user.setName(resultSet.getString("name"));
+        this.user.setPassword(resultSet.getString("password"));
 
         resultSet.close();
         preparedStatement.close();
         connection.close();
 
-        return user;
+        return this.user;
     }
 }
